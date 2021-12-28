b544ddd98a9d8d1eedba9c551d88356d8011818c
HP123-1 <hinalpandya79@gmail.com>
2021-12-28 22:23:45 +0000

    week-16 automation-cucumber

---
+++ src/test/java/com/automationpractice/cucumber/Hooks.java
@@ -0,0 +1,37 @@
+package com.automationpractice.cucumber;
+
+import com.automationpractice.propertyreader.PropertyReader;
+import com.automationpractice.utility.Utility;
+import com.cucumber.listener.Reporter;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+
+import java.io.IOException;
+
+public class Hooks extends Utility {
+    @Before
+
+    public void setUP() {
+        selectBrowser(PropertyReader.getInstance().getProperty("browser"));
+        Reporter.assignAuthor("Hinal");
+
+
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        if (scenario.isFailed()){
+            String screenShotPath = Utility.getScreenshot(driver, scenario.getName().replace(" " , "_"));
+            try {
+                Reporter.addScreenCaptureFromPath(screenShotPath);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+        closeBrowser();
+    }
+}
+
+
